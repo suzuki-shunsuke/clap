688f419da6fa28b1a6b8d448954e3d18a97a0030
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-07-04 15:27:09 +0900

    feat: add version command

--- clap/cmd/root.go
+++ clap/cmd/root.go
@@ -2,15 +2,17 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/suzuki-shunsuke/clap/clap/constant"
 )
 
 var rootCmd = &cobra.Command{ //nolint:gochecknoglobals
-	Use:   "clap",
-	Short: "clap is a simple installer",
-	Long:  `clap is a simple installer. https://github.com/suzuki-shunsuke/clap`,
+	Use:     "clap",
+	Short:   "clap is a simple installer",
+	Long:    `clap is a simple installer. https://github.com/suzuki-shunsuke/clap`,
+	Version: constant.Version,
 }
 
 func Execute() error {
-	rootCmd.AddCommand(installCmd)
+	rootCmd.AddCommand(installCmd, versionCmd)
 	return rootCmd.Execute()
 }
